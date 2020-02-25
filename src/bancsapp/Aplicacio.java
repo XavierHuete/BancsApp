1eff935d4dc3b88cb11ae6b225c26c3c9ae17858
Xavier Huete Casanovas <xavihc13@gmail.com>
2020-02-25 10:46:38 +0100

    Hola holita

--- src/bancsapp/Aplicacio.java
+++ src/bancsapp/Aplicacio.java
@@ -4,7 +4,7 @@ public class Aplicacio {
 
     public static void main(String[] args) {
 
-        System.err.println("Hola holita vencinito.");
+        System.err.println("Hola holita vencinito...");
         
         System.out.println("Introdueix el nom del compte bancari 1: ");
         String nom = cLlegir.dato();
